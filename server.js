72dbd6fdfd1dc526248582b85426dde601a4522d
forhads <osmanforhad94@gmail.com>
2021-10-12 23:10:19 +0600

    setup requre dependencis and connect with mongo DB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
+app.use(morgan('dev'))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.json({
         message: "Welcome To Our Application"
@@ -11,4 +21,9 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log(`Server is Running on Port ${PORT}`)
+    mongoose.connect('mongodb://localhost:27017/money-management-app',
+        { useNewUrlParser: true },
+        () => {
+            console.log('Database Connected...')
+        });
 })
