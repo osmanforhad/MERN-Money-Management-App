ed17bf6783ff7a89c47ff1809631564058812fc8
forhads <osmanforhad94@gmail.com>
2021-10-13 20:58:12 +0600

    working with user registration module and bcrypt user password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcrypt')
 const registerValidator = require('../validator/registerValidator')
+const User = require('../model/User')
 
 // login controller
 module.exports = {
@@ -9,17 +11,59 @@ module.exports = {
             message: `Welcome ${name}, we will contact with you by ${email}`
         })
     },
+    // register controller
     register: (req, res) => {
-        // red client data
+        // read client data or destructure
         let { name, email, password, confirmPassword } = req.body
         // check user data validation
         let validate = registerValidator({ name, email, password, confirmPassword })
         if (!validate.isValid) {
             res.status(400).json(validate.error)
         } else {
-            res.status(200).json({
-                message: "Everything is okey"
-            })
+            // check duplicate user is exists by user proivided email
+            User.findOne({ email })
+                .then(user => {
+                    if (user) {
+                        return res.status(400).json({
+                            messgae: "Email alreday exists"
+                        })
+                    }
+
+                    // passpord Hasing or bcrypt password
+                    bcrypt.hash(password, 11, (err, hash) => {
+                        if (err) {
+                            return res.status(500).json({
+                                message: "Server Error Occurred"
+                            })
+                        }
+                        // create new user
+                        let user = new User({
+                            name,
+                            email,
+                            password: hash
+                        })
+                        // save or rgister user
+                        user.save()
+                            .then(user => {
+                                res.status(201).json({
+                                    message: "Registration Successfull",
+                                    user
+                                })
+                            })
+                            .catch(error => {
+                                console.log(error)
+                                res.status(500).json({
+                                    message: "Server Error Occurred"
+                                })
+                            })
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                    res.status(500).json({
+                        message: "Server Error Occurred"
+                    })
+                })
         }
     }
 }
