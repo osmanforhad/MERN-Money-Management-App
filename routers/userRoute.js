a476198217266d2fec6654d6a5d10598dc8049e9
forhads <osmanforhad94@gmail.com>
2021-10-13 18:56:45 +0600

    testing api through POST Man testing tool

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -9,7 +9,11 @@ router.post('/register', (req, res) => {
 // Login Route
 // localhost:4000/api/users/login
 router.post('/login', (req, res) => {
-
+    let name = req.body.name
+    let email = req.body.email
+    res.json({
+        message: `Welcome ${name}, we will contact with you by ${email}`
+    })
 })
 
 module.exports = router
