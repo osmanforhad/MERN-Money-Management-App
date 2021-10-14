cbb29af796ca9fe52a505764741fdc3009f27a8a
forhads <osmanforhad94@gmail.com>
2021-10-14 11:26:28 +0600

    design login and register page using jsx

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
+class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+        error: {}
+    }
 
-const Login = (props) => {
-    return (
-        <h1>This is the Login Page</h1>
-    )
+    changeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
+    render() {
+        let { email, password, error } = this.state
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h1 className="text-center display-4">Login Here</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" className="form-control" placeholder="Enter Your Email"
+                                name="email" id="email" value={email} onChange={this.changeHandler} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" className="form-control" placeholder="Enter Your Password"
+                                name="password" id="password" value={password} onChange={this.changeHandler} />
+                        </div>
+                        <Link to='/register'>Don't have Account? Register Here</Link>
+                        <button className="btn btn-primary my-3 d-block">Login</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
 
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
+class Register extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        error: {}
+    }
 
-const Register = (props) => {
-    return (
-        <h1>This is the Register Page</h1>
-    )
+    changeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
+    render() {
+        let { name, email, password, confirmPassword, error } = this.state
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h1 className="text-center display-4">Register Here</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <div className="form-group">
+                            <label htmlFor="name">Name:</label>
+                            <input type="text" className="form-control" placeholder="Enter Your Name"
+                                name="name" id="name" value={name} onChange={this.changeHandler} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label>
+                            <input type="email" className="form-control" placeholder="Enter Your Email"
+                                name="email" id="email" value={email} onChange={this.changeHandler} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" className="form-control" placeholder="Enter Your Password"
+                                name="password" id="password" value={password} onChange={this.changeHandler} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="confirmPassword">Confirm Password:</label>
+                            <input type="password" className="form-control" placeholder="Confirm your Password"
+                                name="confirmPassword" id="confirmPassword" value={confirmPassword} onChange={this.changeHandler} />
+                        </div>
+                        <Link to='/login'>Alreday have a Account? Login Here</Link>
+                        <button className="btn btn-primary my-3 d-block">Register</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
 
 
